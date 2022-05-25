07deaf2cf86ef5e8402f5c285417c12c4c140b2c
gaelle jopwou <jgaelle@yahoo.fr>
2022-05-25 14:58:56 +0200

    reservation faite

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,17 @@
 class ReservationsController < ApplicationController
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @atelier = Atelier.find(params[:atelier_id])
+    @reservation.atelier = @atelier
+    @reservation.user = current_user
+    @reservation.save
+    redirect_to dashboard_path(current_user)
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date)
+  end
 end
