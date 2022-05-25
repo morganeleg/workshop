b078015c049c5dd309311fe8093387c572b5d03f
morgane le gall[D <morganeleg@gmail.com>
2022-05-25 14:58:04 +0200

    formulaires new et edit

--- app/controllers/ateliers_controller.rb
+++ app/controllers/ateliers_controller.rb
@@ -2,8 +2,20 @@ class AteliersController < ApplicationController
   before_action :set_atelier, only: [:show, :edit, :update, :destroy]
 
   def index
-    @ateliers = Atelier.all
+    @ateliers = policy_scope(Atelier)
+  end
 
+  def new
+    @atelier = Atelier.new
+    authorize @atelier
+  end
+
+  def create
+    @atelier = Atelier.new(list_params)
+    @atelier.user = current_user
+    @atelier.save
+    authorize @atelier
+    redirect_to atelier_path(@atelier)
   end
 
   def show
@@ -15,4 +27,9 @@ class AteliersController < ApplicationController
   def set_atelier
     @atelier = Atelier.find(params[:id])
   end
+
+  def list_params
+    params.require(:atelier).permit(:name, :address, :style, :details, :price_per_day, photos: [])
+  end
+
 end
--- app/policies/atelier_policy.rb
+++ app/policies/atelier_policy.rb
@@ -12,4 +12,13 @@ class AtelierPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
 end
