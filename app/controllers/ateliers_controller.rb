f5df476001f07ed690d68797e7838f7264e6a204
Wissam ECHO <wissamecho@hotmail.com>
2022-05-27 15:38:41 +0200

    final

--- app/controllers/ateliers_controller.rb
+++ app/controllers/ateliers_controller.rb
@@ -1,6 +1,6 @@
 class AteliersController < ApplicationController
   before_action :set_atelier, only: [:show, :edit, :update, :destroy]
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @ateliers = policy_scope(Atelier)
